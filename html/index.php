88c9ef8f90afa1de5ea840797971c0eed264511c
mi-cawber <waltwest@outlook.com>
2026-08-15 20:29:09 -0400

    added form to subscribe to the email newsletter

--- html/index.php
+++ html/index.php
@@ -6,6 +6,34 @@
     <link rel='stylesheet' href='css/styles.css'>
 </head>
 
+<style>
+    /* Tooltip container */
+    .tooltip {
+      position: relative;
+      display: inline-block;
+      cursor: pointer;
+    }
+
+    /* Tooltip text */
+    .tooltiptext {
+      visibility: hidden; /* Hidden by default */
+      width: 500px;
+      background-color: #f2b76c;
+      color: black;
+      text-align: center;
+      padding: 5px 0;
+      border-radius: 6px;
+      position: absolute;
+      z-index: 1; /* Ensure tooltip is displayed above content */
+    }
+
+    /* Show the tooltip text on hover */
+    .tooltip:hover .tooltiptext {
+        animation: fadeIn 1s forwards;
+        visibility: visible;
+    }
+</style>
+
 <?php
     $updated = "08/15/2026";
 ?>
@@ -43,17 +71,36 @@
         </p>
     </div>
 
+    <div class='tooltip'><p class='body'><a>Subscribe to my email newsletter below!</a></p>
+        <span class='tooltiptext'>My emails will be sent from "venvio@proton.me"</span>
+    </div>
+
+    <form
+      action="https://buttondown.com/api/emails/embed-subscribe/venvio"
+      method="post"
+      class="embeddable-buttondown-form"
+    >
+      <label for="bd-email" class='body'>Email:</label>
+      <input type="email" name="email" id="bd-email" />
+      <input type="submit" value="Subscribe" />
+      <p>
+        <a href="https://buttondown.com/refer/venvio" target="_blank">
+          Powered by Buttondown.
+        </a>
+      </p>
+    </form>
     <!-- bottom container -->
     <div class='col_container'>
         <!-- External Links -->
         <div>
             <p class='body'>
-            <b>External Links:</b>
+            External Links:
             <a href='https://www.youtube.com/@venv_io'>YouTube</a> 
             <a href='https://github.com/venvio'>GitHub</a> 
             <a href='mailto:venvio@proton.me'>Email</a>
             </p>
         </div>
+
 
         <!-- copyright info -->
         <div>
